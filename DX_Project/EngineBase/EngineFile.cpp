87671f2c707d882fcb914779dbc95c47b84515d7
민승 김 <rlaalstmd0606@gmail.com>
2024-04-01 17:28:56 +0900

    중간 커밋

--- DX_Project/EngineBase/EngineFile.cpp
+++ DX_Project/EngineBase/EngineFile.cpp
@@ -1,5 +1,4 @@
 #include "PreCompile.h"
-#include "PreCompile.h"
 #include "EngineFile.h"
 #include "EngineDebug.h"
 #include "EngineSerializer.h"
@@ -101,4 +100,14 @@ void UEngineFile::Load(UEngineSerializer& _Data)
 
 	_Data.BufferResize(static_cast<int>(Size));
 	fread(&_Data.Data[0], Size, 1, FileHandle);
+}
+
+std::string UEngineFile::GetString()
+{
+	Open(EIOOpenMode::Read, EIODataType::Text);
+	UEngineSerializer Ser;
+	Load(Ser);
+	Close();
+
+	return Ser.ToString();
 }
