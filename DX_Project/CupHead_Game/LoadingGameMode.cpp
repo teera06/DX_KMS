7cba1d3cbfb5ad3e0394175b2b8fd5fa2eebd39c
민승 김 <rlaalstmd0606@gmail.com>
2024-04-08 10:52:50 +0900

    중간 커밋

--- DX_Project/CupHead_Game/LoadingGameMode.cpp
+++ DX_Project/CupHead_Game/LoadingGameMode.cpp
@@ -11,16 +11,20 @@ ALoadingGameMode::~ALoadingGameMode()
 
 void ALoadingGameMode::BeginPlay()
 {
+	Super::BeginPlay();
 }
 
 void ALoadingGameMode::Tick(float _DeltaTime)
 {
+	Super::Tick(_DeltaTime);
 }
 
 void ALoadingGameMode::LevelEnd(ULevel* _NextLevel)
 {
+	Super::LevelEnd(_NextLevel);
 }
 
 void ALoadingGameMode::LevelStart(ULevel* _PrevLevel)
 {
+	Super::LevelStart(_PrevLevel);
 }
