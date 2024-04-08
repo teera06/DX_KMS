c0ba657342d9d931ca7b4932b414126fc9835da4
민승 김 <rlaalstmd0606@gmail.com>
2024-04-08 10:46:53 +0900

    중간 커밋

---
+++ DX_Project/CupHead_Game/LoadingGameMode.cpp
@@ -0,0 +1,26 @@
+#include "PreCompile.h"
+#include "LoadingGameMode.h"
+
+ALoadingGameMode::ALoadingGameMode()
+{
+}
+
+ALoadingGameMode::~ALoadingGameMode()
+{
+}
+
+void ALoadingGameMode::BeginPlay()
+{
+}
+
+void ALoadingGameMode::Tick(float _DeltaTime)
+{
+}
+
+void ALoadingGameMode::LevelEnd(ULevel* _NextLevel)
+{
+}
+
+void ALoadingGameMode::LevelStart(ULevel* _PrevLevel)
+{
+}
---
+++ DX_Project/CupHead_Game/LoadingGameMode.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <EngineCore/GameMode.h>
+class ALoadingGameMode : public AGameMode
+{
+	GENERATED_BODY(AGameMode)
+public:
+	// constrcuter destructer
+	ALoadingGameMode(); // 디폴트 생성자
+	~ALoadingGameMode(); // 디폴트 소멸자
+
+	// delete Function
+	ALoadingGameMode(const ALoadingGameMode& _Other) = delete; // 디폴트 복사 생성자
+	ALoadingGameMode(ALoadingGameMode&& _Other) noexcept = delete; 
+	ALoadingGameMode& operator=(const ALoadingGameMode& _Other) = delete; // 디폴트 대입 연산자
+	ALoadingGameMode& operator=(ALoadingGameMode&& _Other) noexcept = delete;
+
+protected:
+	void BeginPlay() override;
+	void Tick(float _DeltaTime) override;
+
+	void LevelEnd(ULevel* _NextLevel);
+	void LevelStart(ULevel* _PrevLevel);
+private:
+};
+
