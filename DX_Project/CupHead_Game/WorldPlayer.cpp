7e15cd6e88c07131cb881c27e2acba849c7200f5
민승 김 <rlaalstmd0606@gmail.com>
2024-04-08 17:19:24 +0900

    중간 커밋

--- DX_Project/CupHead_Game/WorldPlayer.cpp
+++ DX_Project/CupHead_Game/WorldPlayer.cpp
@@ -4,6 +4,7 @@
 AWorldPlayer::AWorldPlayer()
 {
 	WorldPlayerRenderer = CreateDefaultSubObject<USpriteRenderer>("WorldPlayer");
+	InputOn();
 }
 
 AWorldPlayer::~AWorldPlayer()
@@ -28,4 +29,31 @@ void AWorldPlayer::BeginPlay()
 void AWorldPlayer::Tick(float _DeltaTime)
 {
 	Super::Tick(_DeltaTime);
+	float Speed = 100.0f;
+
+	if (true == IsPress('A'))
+	{
+		AddActorLocation(FVector::Left * _DeltaTime * Speed);
+	}
+
+	if (true == IsPress('D'))
+	{
+		AddActorLocation(FVector::Right * _DeltaTime * Speed);
+	}
+
+	if (true == IsPress('W'))
+	{
+		AddActorLocation(FVector::Up * _DeltaTime * Speed);
+	}
+
+	if (true == IsPress('S'))
+	{
+		AddActorLocation(FVector::Down * _DeltaTime * Speed);
+	}
+
+	if (true == IsPress(VK_NUMPAD1))
+	{
+		// AddActorRotation(float4{0.0f, 0.0f, 1.0f} * 360.0f * _DeltaTime);
+		// Color.X += _DeltaTime;
+	}
 }
