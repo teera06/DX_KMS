3a157c31fbf25c155f55e5300029873b93b07755
민승 김 <rlaalstmd0606@gmail.com>
2024-04-08 10:00:50 +0900

    중간 커밋

--- DX_Project/CupHead_Game/WorldGameMode.cpp
+++ DX_Project/CupHead_Game/WorldGameMode.cpp
@@ -11,16 +11,20 @@ WorldGameMode::~WorldGameMode()
 
 void WorldGameMode::BeginPlay()
 {
+	Super::BeginPlay();
 }
 
 void WorldGameMode::Tick(float _DeltaTime)
 {
+	Super::Tick(_DeltaTime);
 }
 
 void WorldGameMode::LevelEnd(ULevel* _NextLevel)
 {
+	Super::LevelEnd(_NextLevel);
 }
 
 void WorldGameMode::LevelStart(ULevel* _PrevLevel)
 {
+	Super::LevelStart(_PrevLevel);
 }
