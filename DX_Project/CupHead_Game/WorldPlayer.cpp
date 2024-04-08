4c4d70a1914d7603a00582a732c605ae3e1a7251
민승 김 <rlaalstmd0606@gmail.com>
2024-04-08 16:56:12 +0900

    중간 커밋

--- DX_Project/CupHead_Game/WorldPlayer.cpp
+++ DX_Project/CupHead_Game/WorldPlayer.cpp
@@ -14,7 +14,7 @@ void AWorldPlayer::BeginPlay()
 {
 	Super::BeginPlay();
 	SetActorScale3D(FVector(60.0f, 100.0f, 100.0f));
-	SetActorLocation(FVector(0.0f, 0.0f, 50.0f));
+	SetActorLocation(FVector(0.0f, 0.0f, -50.0f));
 
 	//DelayCallBack(1.0f, std::bind(&ATitleLogo::RendererOff, this));
 	WorldPlayerRenderer->SetSprite("WorldDiagonalIdle_01.png");
