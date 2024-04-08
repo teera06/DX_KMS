697a8010d88ec3fb0619e44e91835695520e32c8
민승 김 <rlaalstmd0606@gmail.com>
2024-04-08 13:57:11 +0900

    중간 커밋

--- DX_Project/CupHead_Game/MainTitleActor.cpp
+++ DX_Project/CupHead_Game/MainTitleActor.cpp
@@ -13,11 +13,10 @@ AMainTitleActor::~AMainTitleActor()
 void AMainTitleActor::BeginPlay()
 {
 	Super::BeginPlay();
-
-	SetActorScale3D(FVector(1280.0f, 720.0f, 100.0f));
+	SetActorLocation(FVector(0.0f, -50.0f, 0.0f));
+	SetActorScale3D(FVector(1100.0f, 620.0f, 100.0f));
 
 	//DelayCallBack(1.0f, std::bind(&ATitleLogo::RendererOff, this));
-
 	Renderer->SetSprite("cuphead_title_screen_0001.png");
 	Renderer->SetSamplering(ETextureSampling::LINEAR);
 
