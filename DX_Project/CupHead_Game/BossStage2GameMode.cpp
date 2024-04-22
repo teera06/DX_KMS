1502c64242fb74f46ec1595ab306e41a45e3bafc
민승 김 <rlaalstmd0606@gmail.com>
2024-04-22 09:59:04 +0900

    중간 커밋

---
+++ DX_Project/CupHead_Game/BossStage2GameMode.cpp
@@ -0,0 +1,26 @@
+#include "PreCompile.h"
+#include "BossStage2GameMode.h"
+
+BossStage2GameMode::BossStage2GameMode()
+{
+}
+
+BossStage2GameMode::~BossStage2GameMode()
+{
+}
+
+void BossStage2GameMode::BeginPlay()
+{
+}
+
+void BossStage2GameMode::Tick(float _DeltaTime)
+{
+}
+
+void BossStage2GameMode::LevelEnd(ULevel* _NextLevel)
+{
+}
+
+void BossStage2GameMode::LevelStart(ULevel* _PrevLevel)
+{
+}
---
+++ DX_Project/CupHead_Game/BossStage2GameMode.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <EngineCore/GameMode.h>
+
+class BossStage2GameMode : public AGameMode
+{
+public:
+	// constrcuter destructer
+	BossStage2GameMode(); // 디폴트 생성자
+	~BossStage2GameMode(); // 디폴트 소멸자
+
+	// delete Function
+	BossStage2GameMode(const BossStage2GameMode& _Other) = delete; // 디폴트 복사 생성자
+	BossStage2GameMode(BossStage2GameMode&& _Other) noexcept = delete; 
+	BossStage2GameMode& operator=(const BossStage2GameMode& _Other) = delete; // 디폴트 대입 연산자
+	BossStage2GameMode& operator=(BossStage2GameMode&& _Other) noexcept = delete;
+
+protected:
+	void BeginPlay() override;
+	void Tick(float _DeltaTime) override;
+
+	void LevelEnd(ULevel* _NextLevel);
+	void LevelStart(ULevel* _PrevLevel);
+private:
+};
+
