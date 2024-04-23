ef05c81100364f6dde370bfc41c21e6e8e1a37fe
민승 김 <rlaalstmd0606@gmail.com>
2024-04-23 10:57:49 +0900

    중간 커밋

---
+++ DX_Project/CupHead_Game/DevilBall.cpp
@@ -0,0 +1,18 @@
+#include "PreCompile.h"
+#include "DevilBall.h"
+
+ADevilBall::ADevilBall()
+{
+}
+
+ADevilBall::~ADevilBall()
+{
+}
+
+void ADevilBall::BeginPlay()
+{
+}
+
+void ADevilBall::Tick(float _DeltaTime)
+{
+}
---
+++ DX_Project/CupHead_Game/DevilBall.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <EngineCore/Actor.h>
+
+class USpriteRenderer;
+
+class ADevilBall : public AActor
+{
+public:
+	// constrcuter destructer
+	ADevilBall(); // 디폴트 생성자
+	~ADevilBall(); // 디폴트 소멸자
+
+	// delete Function
+	ADevilBall(const ADevilBall& _Other) = delete; // 디폴트 복사 생성자
+	ADevilBall(ADevilBall&& _Other) noexcept = delete; 
+	ADevilBall& operator=(const ADevilBall& _Other) = delete; // 디폴트 대입 연산자
+	ADevilBall& operator=(ADevilBall&& _Other) noexcept = delete;
+
+protected:
+	void BeginPlay() override;
+	void Tick(float _DeltaTime) override;
+private:
+	USpriteRenderer* headatt = nullptr;
+};
+
