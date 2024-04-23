1d609365dfe211e56d88d4004e52dfa58cdd9338
민승 김 <rlaalstmd0606@gmail.com>
2024-04-23 16:21:15 +0900

    중간 커밋

--- DX_Project/CupHead_Game/Boss2Phase2Map.cpp
+++ DX_Project/CupHead_Game/Boss2Phase2Map.cpp
@@ -14,10 +14,11 @@ ABoss2Phase2Map::ABoss2Phase2Map()
 	Boss2Phase2Map->SetupAttachment(Root);
 	
 	Boss2Phase2Map->SetOrder(ERenderOrder::Boss1_back3);
-	Boss2Phase2Map->SetSprite("BossDevil_Phase2Background.png");
+	Boss2Phase2Map->SetSprite("BossDevil_Phase1Background.png");
 	Boss2Phase2Map->SetSamplering(ETextureSampling::LINEAR);
 	Boss2Phase2Map->SetAutoSize(1.2f, true);
 
+	
 	SetRoot(Root);
 }
 
@@ -28,6 +29,13 @@ ABoss2Phase2Map::~ABoss2Phase2Map()
 void ABoss2Phase2Map::BeginPlay()
 {
 	Super::BeginPlay();
+
+	Boss2Phase2Map->SetScale(FVector(1280.0f, 720.0f, 0.0f));
+	Boss2Phase2Map->CreateAnimation("LastMap", "BossDevil_Phase1Background.png", 0.01f,false, 4, 5);
+
+	Boss2Phase2Map->ChangeAnimation("LastMap");
+
+
 	SetActorLocation(FVector(0.0f, 0.0f, 200.0f));
 }
 
