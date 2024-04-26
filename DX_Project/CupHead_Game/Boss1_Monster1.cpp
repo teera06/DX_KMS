b793726a5cbe92e3d0edd064795b88e5000ea332
민승 김 <rlaalstmd0606@gmail.com>
2024-04-26 09:59:07 +0900

    중간 커밋

--- DX_Project/CupHead_Game/Boss1_Monster1.cpp
+++ DX_Project/CupHead_Game/Boss1_Monster1.cpp
@@ -39,7 +39,7 @@ void ABoss1_Monster1::BeginPlay()
 	SmallBoss1->SetOrder(ERenderOrder::Monster2);
 	SmallBoss1->SetSprite("shortFrog_idle_0001.png");
 	SmallBoss1->SetSamplering(ETextureSampling::LINEAR);
-	SmallBoss1->SetPlusColor(FVector(0.2f, 0.2f, 0.2f));
+	SmallBoss1->SetPlusColor(FVector(0.1f, 0.1f, 0.1f));
 	AniCreate();
 
 	Phase1StateInit();
--- DX_Project/CupHead_Game/Boss1_Monster2.cpp
+++ DX_Project/CupHead_Game/Boss1_Monster2.cpp
@@ -115,7 +115,7 @@ void ABoss1_Monster2::BeginPlay()
 	BigBoss1->SetOrder(ERenderOrder::Monster1);
 	BigBoss1->SetSprite("tallfrog_idle_0001.png");
 	BigBoss1->SetSamplering(ETextureSampling::LINEAR);
-	BigBoss1->SetPlusColor(FVector(0.18f, 0.18f, 0.13f));
+	BigBoss1->SetPlusColor(FVector(0.1f, 0.1f, 0.1f));
 
 	SlotMouse->SetOrder(ERenderOrder::SlotMouse);
 	SlotMouse->SetSprite("tallfrog_slotman_spit_0001.png");
