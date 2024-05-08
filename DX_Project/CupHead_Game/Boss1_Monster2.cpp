bd97073fc76746843765136c43f379220470ffdf
민승 김 <rlaalstmd0606@gmail.com>
2024-05-08 11:17:21 +0900

    중간 커밋

--- DX_Project/CupHead_Game/Boss1_Monster2.cpp
+++ DX_Project/CupHead_Game/Boss1_Monster2.cpp
@@ -346,7 +346,7 @@ void ABoss1_Monster2::AniCreate()
 	BigBoss1->CreateAnimation("Phase3SlotStart", "Phase3SlotReady", 0.1f, false, 8, 0);
 
 	BigBoss1->CreateAnimation("Phase3AttReady", "Phase3AttReady", 0.075f);
-	BigBoss1->CreateAnimation("Phase3Att", "Phase3Att", 0.0752f);
+	BigBoss1->CreateAnimation("Phase3Att", "Phase3Att", 0.075f);
 	BigBoss1->CreateAnimation("Phase3AttEnd", "Phase3AttReady", 0.075f,false,6,0);
 
 	BigBoss1->CreateAnimation("DieTransition", "DieTransition", 0.075f);
